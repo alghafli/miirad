9f9a9c811a0d6252f90899448f2b48c1328193a0
alghafli <37610446+alghafli@users.noreply.github.com>
2021-04-16 11:06:55 +0300

    Add files via upload

--- miirad/utils.py
+++ miirad/utils.py
@@ -1,4 +1,11 @@
 from functools import partial
+from sqlalchemy import create_engine
+import shutil
+
+try:
+    import netifaces
+except ImportError:
+    netifaces = None
 
 def caller(*args):
     for c in args:
@@ -6,3 +13,10 @@ def caller(*args):
 
 def partial_caller(*args):
     return partial(caller, *args)
+
+def backup(f, new_f):
+    engine = create_engine('sqlite:///{}'.format(f))
+    with engine.connect() as con:
+        con.execute('BEGIN IMMEDIATE')
+        shutil.copy(f, new_f)
+
