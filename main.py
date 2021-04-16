b8134262262dcb0c8eacdfed3897447745e0eb1b
alghafli <37610446+alghafli@users.noreply.github.com>
2021-04-16 11:06:36 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 from pathlib import Path
 from cofan import Filer, BaseProvider, Server, BaseHandler
-from miirad import providers
+from miirad import providers, utils
 from miirad.providers import *
+import time
+import argparse
 
-working_dir = Path(__file__).parent
+parser = argparse.ArgumentParser(
+    description='Miirad server. Track your incomes and expenses')
+parser.add_argument(
+    '-a', '--addr', type=str, action='store', default='localhost:8000',
+    help='server address in the form <ip> or <ip>:<port>'
+)
+parser.add_argument(
+    '-c', '--config', type=str, action='store',
+    default=str(Path(__file__).parent / 'config'),
+    help='configuration directory'
+)
+
+args = parser.parse_args()
+
+addr = args.addr.split(':')
+if len(addr) > 1:
+    addr, port = addr
+    port = int(port)
+else:
+    addr = addr[0]
+    port = 8000
+
+config_dir = Path(args.config)
 miirad_dir = Path(providers.__file__).parent
 
 #after selecting db
@@ -17,15 +41,15 @@ db_patterner.add('edit_categories$', CategoryEditor)
 db_patterner.add('$', Indexer)
 
 #after getting session
-edit_db = DBEditor(working_dir / 'config')
-db_selector = DBSelector(working_dir / 'config', db_patterner)
+edit_db = DBEditor(config_dir)
+db_selector = DBSelector(config_dir, db_patterner)
 
 session_patterner = PostPatterner()
 session_patterner.add('edit_db$', edit_db)
 session_patterner.add('settings$', SettingsViewer)
-session_patterner.add('db_list$', DbLister(working_dir / 'config'))
-session_patterner.add('change_db$', DBChanger(working_dir / 'config'))
-session_patterner.add('copy_db$', DBCopier(working_dir / 'config'))
+session_patterner.add('db_list$', DbLister(config_dir))
+session_patterner.add('change_db$', DBChanger(config_dir))
+session_patterner.add('copy_db$', DBCopier(config_dir))
 session_patterner.add('', db_selector)
 
 
@@ -35,7 +59,7 @@ session_provider = Sessioner(session_patterner)
 
 toplevel = PostPatterner()
 toplevel.add('__assets__/', assets)
-toplevel.add('about$', Filer(miirad_dir / 'data/html/about.html'))
+toplevel.add('about$', Texter(miirad_dir / 'data/html/about.html'))
 toplevel.add('', session_provider)
 
 templater = Templater(
@@ -43,11 +67,32 @@ templater = Templater(
 
 handler = PostHandler(templater)
 
-srv = Server(('localhost', 8000), handler)
+srv = Server((addr, port), handler)
 
 try:
     srv.serve_forever()
 except KeyboardInterrupt:
     pass
 
+
+dbp = config_dir / 'db'
+bup = config_dir / 'backup'
+bup.mkdir(parents=True, exist_ok=True)
+t = int(time.time())
+for f in dbp.iterdir():
+    name = f.name
+    tmp_f = bup / 'temp_{}'.format(f.name)
+    bu_f = bup / '{}_{}.sqlite3'.format(f.stem, t)
+    print('creating {} backup'.format(f.stem))
+    try:
+        utils.backup(f, tmp_f)
+        tmp_f.rename(bu_f)
+    except:
+        print('failed to backup {}'.format(f.name))
+        tmp_f.unlink(missing_ok=True)
+    
+    c = sorted(bup.glob('{}_*'.format(f.stem)),
+        key=lambda f: f.stem.split('_', 1)[1], reverse=True)
+    while len(c) > 10:
+        c.pop().unlink(missing_ok=True)
 
